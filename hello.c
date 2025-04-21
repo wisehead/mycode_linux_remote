912d4d4006aa61fa784d845fd3ab1054f11ea607
wisehead <alex.chenhui@gmail.com>
2025-04-21 16:50:02 +0800

    update vscode

--- hello.c
+++ hello.c
@@ -6,6 +6,6 @@
  * modified time: 25/02/13- 2:02                                    
  *******************************************************************************/
 #include <stdio.h>
-void main() {
+int main() {
 	printf("Hello World!\n");
 }
